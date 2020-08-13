6a532af38abd4d4750754b7812525ae434466b69
pro-dev12 <adev22321@gmail.com>
2020-08-13 17:24:54 +0300

    refactoring

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NzModalModule } from 'ng-zorro-antd/modal';
-import { NavbarComponent, DashboardComponent, AppComponent } from './components';
+import { NavbarComponent, DashboardComponent, AppComponent, DragDrawerComponent } from './components';
 import { RouterModule } from '@angular/router';
 import { LayoutModule } from 'layout';
 import { LoadingModule } from 'lazy-modules';
 import { FakeCommunicationModule } from 'fake-communication';
-import {DragDrawerComponent} from './components/dashboard/drag-drawer/drag-drawer.component';
 
 @NgModule({
   declarations: [
