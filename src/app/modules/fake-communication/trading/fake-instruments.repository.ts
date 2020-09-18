fcfd094a7d86cd4ae82ff3981c5847447019a2d2
pro-dev12 <adev22321@gmail.com>
2020-09-18 09:31:13 +0300

    Disable moving in any directions, split instruments

--- src/app/modules/fake-communication/trading/fake-instruments.repository.ts
+++ src/app/modules/fake-communication/trading/fake-instruments.repository.ts
@@ -3,10 +3,10 @@ import { FakeRepository } from "../common";
 
 export class FakeInstrumentsRepository extends FakeRepository<IInstrument> {
 
-    protected async _getItems() {
-        const symbolsFilePath = './assets/instruments.json';
-        const response = await fetch(symbolsFilePath);
-        const data = await response.json();
-        return data.map(i => ({ tickSize: 0.01, id: i.symbol, name: i.symbol, ...i }));
-    }
+  protected async _getItems() {
+    const symbolsFilePath = './assets/instruments.json';
+    const response = await fetch(symbolsFilePath);
+    const data = await response.json();
+    return data.map(i => ({ tickSize: 0.01, id: i.symbol, name: i.symbol, ...i })).splice(0, 100);
+  }
 }
