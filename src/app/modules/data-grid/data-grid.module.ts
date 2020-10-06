ee7e3cbd8d05a57f441746dc3ce7e359887f1cb0
pro-dev12 <adev22321@gmail.com>
2020-10-06 08:59:27 +0300

    Issue_#30:Resizing

--- src/app/modules/data-grid/data-grid.module.ts
+++ src/app/modules/data-grid/data-grid.module.ts
@@ -1,10 +1,12 @@
-import { DataGrid } from './components/data-grid/data-grid.component';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { ItemViewBuilder } from './components/item-view-builder';
 import { ScrollingModule } from '@angular/cdk/scrolling';
+import { NzTransferModule } from 'ng-zorro-antd/transfer';
+import { NzMenuModule } from 'ng-zorro-antd/menu';
+import { DataGrid } from './components/data-grid/data-grid.component';
+import { ItemViewBuilder } from './components/item-view-builder';
 import { IconComponent } from './models/cells/components/icon-conponent';
-
+import { ResizebleDirective } from './directives/resizeble.directive';
 const entryComponents = [
   DataGrid,
   IconComponent,
@@ -14,10 +16,12 @@ const entryComponents = [
   imports: [
     CommonModule,
     ScrollingModule,
+    NzTransferModule,
+    NzMenuModule
   ],
   declarations: [
     ItemViewBuilder,
-
+    ResizebleDirective,
     ...entryComponents,
   ],
   entryComponents,
---
+++ src/app/modules/data-grid/directives/resizeble.directive.ts
@@ -0,0 +1,93 @@
+import {Directive, ElementRef, Input, OnInit} from '@angular/core';
+
+@Directive({
+  selector: '[appResizeble]'
+})
+export class ResizebleDirective implements OnInit{
+
+
+  @Input() resizableGrabWidth = 1;
+  @Input() resizableMinWidth = 70;
+
+  dragging = false;
+
+  constructor(private el: ElementRef) {
+
+    const self = this;
+
+    const EventListenerMode = { capture: true };
+
+    function preventGlobalMouseEvents() {
+      document.body.style['pointer-events'] = 'none';
+    }
+
+    function restoreGlobalMouseEvents() {
+      document.body.style['pointer-events'] = 'auto';
+    }
+
+
+    const newWidth = (wid) => {
+      const newWidth = Math.max(this.resizableMinWidth, wid);
+      el.nativeElement.style.width = (newWidth) + 'px';
+    };
+
+
+    const mouseMoveG = (evt) => {
+      if (!this.dragging) {
+        return;
+      }
+      newWidth(evt.clientX - el.nativeElement.offsetLeft);
+      evt.stopPropagation();
+    };
+
+    const dragMoveG = (evt) => {
+      if (!this.dragging) {
+        return;
+      }
+      const newWidth = Math.max(this.resizableMinWidth, (evt.clientX - el.nativeElement.offsetLeft)) + 'px';
+      el.nativeElement.style.width = (evt.clientX - el.nativeElement.offsetLeft) + 'px';
+      evt.stopPropagation();
+    };
+
+    const mouseUpG = (evt) => {
+      if (!this.dragging) {
+        return;
+      }
+      restoreGlobalMouseEvents();
+      this.dragging = false;
+      evt.stopPropagation();
+    };
+
+    const mouseDown = (evt) => {
+      if (this.inDragRegion(evt)) {
+        this.dragging = true;
+        preventGlobalMouseEvents();
+        evt.stopPropagation();
+      }
+    };
+
+
+    const mouseMove = (evt) => {
+      if (this.inDragRegion(evt) || this.dragging) {
+        el.nativeElement.style.cursor = 'col-resize';
+      } else {
+        el.nativeElement.style.cursor = 'default';
+      }
+    };
+
+
+    document.addEventListener('mousemove', mouseMoveG, true);
+    document.addEventListener('mouseup', mouseUpG, true);
+    el.nativeElement.addEventListener('mousedown', mouseDown, true);
+    el.nativeElement.addEventListener('mousemove', mouseMove, true);
+  }
+
+  ngOnInit(): void {
+    this.el.nativeElement.style['border-left'] = this.resizableGrabWidth + 'px solid lightslategray';
+    this.el.nativeElement.style['border-right'] = this.resizableGrabWidth + 'px solid lightslategray';
+  }
+
+  inDragRegion(evt) {
+    return this.el.nativeElement.clientWidth - evt.clientX + this.el.nativeElement.offsetLeft < this.resizableGrabWidth;
+  }
+}
