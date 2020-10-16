f004bc8d87ac02ca8f06eb54a62ae49bb9ac0eee
pro-dev12 <adev22321@gmail.com>
2020-10-16 13:04:48 +0000

    feat(global-handler): create communication logic
    1. Create public method "on", which storage "executor" by "eventType"
    2. Create public method "emit", which call executors in popup windows
    3. Realize private method "_handlePopupEvent"
    4. Refactor storage of types, constants and interfaces

---
+++ projects/global-handler/src/global-handler.module.ts
@@ -0,0 +1,7 @@
+import { NgModule } from '@angular/core';
+import { GlobalHandlerService } from './global-handler.service';
+
+@NgModule({
+  providers: [GlobalHandlerService]
+})
+export class GloabalHandlerModule { }
