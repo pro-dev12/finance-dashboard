6a69f2f7e26d7a384f5c965ee25003ef5f38194f
pro-dev12 <adev22321@gmail.com>
2020-11-02 10:40:22 +0200

    feat(navbar): add navbar-controller component

---
+++ src/app/components/navbar/navbar-controller/navbar-controller.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-navbar-controller',
+  templateUrl: './navbar-controller.component.html',
+  styleUrls: ['./navbar-controller.component.scss']
+})
+export class NavbarControllerComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
