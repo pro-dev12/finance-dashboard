26cf12189d1d0d857976c03309852fa4d4935515
pro-dev12 <adev22321@gmail.com>
2021-03-23 20:17:19 +0200

    remove prototype

--- src/string.ts
+++ src/string.ts
@@ -1,12 +1,7 @@
 interface String {
   capitalize: () => string;
-  isEmpty: () => boolean;
 }
 
 String.prototype.capitalize = function() {
   return this.charAt(0).toUpperCase() + this.slice(1);
-};
-
-String.prototype.isEmpty = function() {
-  return this.length === 0;
 };
