652d9e239335dbe921099897c75ce0b6bbea2429
pro-dev12 <adev22321@gmail.com>
2021-04-15 13:01:48 +0300

    refactor

--- src/app/components/navbar/workspace/workspace.component.ts
+++ src/app/components/navbar/workspace/workspace.component.ts
@@ -127,7 +127,7 @@ export class WorkspaceComponent implements OnInit {
       nzComponentParams: {
         name: 'Name new workspace',
         blankOption: 'Blank workspace',
-        options: [...this.workspaces.map(item => ({ value: item.id, label: item.name }))],
+        options: this.workspaces.map(item => ({ value: item.id, label: item.name })),
       },
     });
 
