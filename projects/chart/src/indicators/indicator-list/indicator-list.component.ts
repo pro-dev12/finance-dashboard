7f3c877d00912a05be44984f78f6ccd7674c393b
pro-dev12 <adev22321@gmail.com>
2021-05-06 22:06:48 +0300

    add indicators description

--- projects/chart/src/indicators/indicator-list/indicator-list.component.ts
+++ projects/chart/src/indicators/indicator-list/indicator-list.component.ts
@@ -1,6 +1,9 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { StringHelper } from 'base-components';
 import { IChart } from 'chart';
 import { ILayoutNode, LayoutNode } from 'layout';
+
+const { capitalize } = StringHelper;
 
 declare const StockChartX: any;
 
@@ -19,6 +22,12 @@ export class IndicatorListComponent implements OnInit {
   chart: IChart;
   link: any;
   showTooltips: boolean;
+  indicatorsDescriptions: {
+    [key: string]: {
+      title: string;
+      content: string[];
+    }[];
+  } = {};
 
   ngOnInit() {
     this.setTabTitle('Add Indicators');
@@ -56,7 +65,31 @@ export class IndicatorListComponent implements OnInit {
     this.close();
   }
 
-  getTitle(item: any) {
-    return item;
+  fetchDescription(name: string) {
+    if (!this.showTooltips || this.indicatorsDescriptions.hasOwnProperty(name)) {
+      return;
+    }
+
+    const keys = ['overview', 'interpretation', 'parameters'];
+    const contentKeys = ['content', 'content1', 'content2'];
+
+    const promises = keys.map(key => {
+      const contentPromises = contentKeys.map(contentKey => {
+        return StockChartX.Localization.localizeText(
+          this.chart,
+          `indicator.${name}.help.${key}.${contentKey}`,
+          { defaultValue: null },
+        );
+      });
+
+      return Promise.all(contentPromises).then(content => content.filter(Boolean));
+    });
+
+    Promise.all(promises).then(sections => {
+      this.indicatorsDescriptions[name] = sections.map((content, i) => ({
+        title: capitalize(keys[i]),
+        content,
+      }));
+    });
   }
 }
