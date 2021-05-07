3fc7da97909b7e3f807ad48a171bc1cd9901eda0
pro-dev12 <adev22321@gmail.com>
2021-05-07 20:05:13 +0300

    base

--- projects/dynamic-form/src/lib/color-picker/color-picker.component.ts
+++ projects/dynamic-form/src/lib/color-picker/color-picker.component.ts
@@ -1,15 +1,25 @@
 import { Component, OnInit } from '@angular/core';
 import { FieldType } from '@ngx-formly/core';
 
+const Palette: string[][] = [
+  ['#FFE8E8', '#F68E8E', '#F16E6E', '#E95050', '#EA3939', '#DD2121', '#CB1212', '#BC0606', '#9D0A0A', '#820303'],
+  ['#FFE0CF', '#FFBB96', '#FFA16D', '#FF8440', '#FF6C1C', '#FF5A00', '#E04F00', '#C04400', '#9C3700', '#802D00'],
+];
+
 @Component({
   selector: 'color-picker',
   templateUrl: './color-picker.component.html',
   styleUrls: ['./color-picker.component.scss']
 })
 export class ColorPickerComponent extends FieldType {
+  readonly palette: string[][] = Palette;
 
   updateValue($event: string) {
     if (!this.formControl.disabled)
       this.formControl.patchValue($event);
+  }
+
+  changeColor(color: string): void {
+    this.formControl.setValue(color);
   }
 }
--- projects/dynamic-form/src/lib/dynamic-form.module.ts
+++ projects/dynamic-form/src/lib/dynamic-form.module.ts
@@ -2,12 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import {
-  NzDatePickerModule,
-  NzDividerModule,
-  NzInputModule,
-  NzInputNumberModule,
-  NzSelectModule,
-  NzSwitchModule
+    NzDatePickerModule,
+    NzDividerModule, NzDropDownModule,
+    NzInputModule,
+    NzInputNumberModule,
+    NzSelectModule,
+    NzSwitchModule
 } from 'ng-zorro-antd';
 import { ColorPickerModule } from 'ngx-color-picker';
 import { FormlyModule } from '@ngx-formly/core';
@@ -57,23 +57,24 @@ const formlyComponents = [
 @NgModule({
   declarations: [ColorPickerComponent, NumberInputComponent, TextAlignComponent,
     SwitchComponent, HotkeyComponent, SelectComponent, ColorSelectComponent, LineSelectorComponent, DatepickerComponent],
-  imports: [
-    CommonModule,
-    ColorPickerModule,
-    ReactiveFormsModule,
-    FormsModule,
-    FormlyModule.forRoot({
-      types: formlyComponents,
-    }),
-    HotkeyInputModule,
-    FormlyNgZorroAntdModule,
-    NzInputModule,
-    NzSwitchModule,
-    NzSelectModule,
-    NzDividerModule,
-    NzInputNumberModule,
-    NzDatePickerModule,
-  ],
+    imports: [
+        CommonModule,
+        ColorPickerModule,
+        ReactiveFormsModule,
+        FormsModule,
+        FormlyModule.forRoot({
+            types: formlyComponents,
+        }),
+        HotkeyInputModule,
+        FormlyNgZorroAntdModule,
+        NzInputModule,
+        NzSwitchModule,
+        NzSelectModule,
+        NzDividerModule,
+        NzInputNumberModule,
+        NzDatePickerModule,
+        NzDropDownModule,
+    ],
   exports: [FormlyModule]
 })
 export class DynamicFormModule {
