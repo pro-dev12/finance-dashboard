76ab0025d96772fd711f103f645c6fd32ebf1462
pro-dev12 <adev22321@gmail.com>
2021-06-24 13:27:51 +0300

    issue_#377: NaN in Positions

--- projects/base-components/src/pipes/fixed-number.pipe.ts
+++ projects/base-components/src/pipes/fixed-number.pipe.ts
@@ -5,11 +5,14 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class FixedNumberPipe implements PipeTransform {
   transform(value: number, precision = 2): string | null {
-    return numberWithCommas(value, precision);
+    return numberWithCommas(value, precision) ?? '';
   }
 }
 
 export function numberWithCommas(value: number, precision: number): string {
+  if (value == null || isNaN(value))
+    return;
+
   return Number(value).toLocaleString('en-US', {
     minimumFractionDigits: precision,
     maximumFractionDigits: precision
