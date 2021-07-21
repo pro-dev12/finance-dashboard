31867783f0bbb1af5154bd7e55e877aaebfb6321
pro-dev12 <adev22321@gmail.com>
2021-07-21 03:04:26 +0300

    add array helper

---
+++ projects/base-components/src/helpers/array.helper.ts
@@ -0,0 +1,7 @@
+export class ArrayHelper {
+  static swapItems(arr: any[], i: number, j: number) {
+    const temp = arr[i];
+    arr[i] = arr[j];
+    arr[j] = temp;
+  }
+}
--- projects/base-components/src/helpers/index.ts
+++ projects/base-components/src/helpers/index.ts
@@ -3,3 +3,4 @@ export * from './string.helper';
 export * from './mixin.helper';
 export * from './dom.helper';
 export * from './object.helper';
+export * from './array.helper';
