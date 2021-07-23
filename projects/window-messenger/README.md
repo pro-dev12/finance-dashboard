1256080fae18fa7cf2c42189a91e4132b2cad648
pro-dev12 <adev22321@gmail.com>
2021-07-23 10:04:53 +0000

    add auto open windows and bug fixes

---
+++ projects/window-messenger/README.md
@@ -0,0 +1,24 @@
+# WindowMessanger
+
+This library was generated with [Angular CLI](https://github.com/angular/angular-cli) version 10.1.3.
+
+## Code scaffolding
+
+Run `ng generate component component-name --project window-messanger` to generate a new component. You can also use `ng generate directive|pipe|service|class|guard|interface|enum|module --project window-messanger`.
+> Note: Don't forget to add `--project window-messanger` or else it will be added to the default project in your `angular.json` file. 
+
+## Build
+
+Run `ng build window-messanger` to build the project. The build artifacts will be stored in the `dist/` directory.
+
+## Publishing
+
+After building your library with `ng build window-messanger`, go to the dist folder `cd dist/window-messanger` and run `npm publish`.
+
+## Running unit tests
+
+Run `ng test window-messanger` to execute the unit tests via [Karma](https://karma-runner.github.io).
+
+## Further help
+
+To get more help on the Angular CLI use `ng help` or go check out the [Angular CLI README](https://github.com/angular/angular-cli/blob/master/README.md).
---
+++ projects/window-messenger/src/window-messenger.service.ts
@@ -0,0 +1,47 @@
+import { Injectable } from '@angular/core';
+
+enum WindowEvents {
+  Message = 'message'
+}
+
+@Injectable({ providedIn: 'root' })
+export class WindowMessengerService {
+  private _listeners = {};
+
+  constructor() {
+    window.addEventListener(WindowEvents.Message, ({ data }) => {
+
+      if (!data || typeof data !== 'string')
+        return;
+
+      try {
+        const { type, payload } = JSON.parse(data);
+        const listener = this._listeners[type];
+        listener?.forEach(fn => fn(payload));
+      } catch (e) {
+        console.log(e);
+      }
+    });
+    window.postMessage(JSON.stringify({ data: 'testType' }), window.location.origin);
+
+  }
+
+  send(type, payload) {
+    try {
+      const message = JSON.stringify({ type, payload });
+      window?.opener.postMessage(message, window.location.origin);
+    } catch (err) {
+      console.error(err);
+    }
+  }
+
+  subscribe(type, listener) {
+    if (!this._listeners[type]?.length) {
+      this._listeners[type] = [];
+    }
+    this._listeners[type].push(listener);
+    return () => {
+      this._listeners[type] = this._listeners[type].filter(item => item !== listener);
+    };
+  }
+}
