d64cbdff11c0ad8e6599c156e11294c5df73fb28
pro-dev12 <adev22321@gmail.com>
2021-09-01 16:14:25 +0300

    connect vwap setting to indicator

--- projects/chart/src/indicators/indicators/VWAP.ts
+++ projects/chart/src/indicators/indicators/VWAP.ts
@@ -1,8 +1,9 @@
+import { zigZagConfig } from '../fields';
 import { Indicator } from './Indicator';
 
 export class VWAP extends Indicator {
   name = 'VWAP';
-  config = {};
+  config = zigZagConfig;
 
   protected _mapGetSettings(settings: any) {
     return settings;
