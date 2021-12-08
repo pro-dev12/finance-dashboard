2cf99dc55d5282b86e9b22acaea88b84273ffa7c
pro-dev12 <adev22321@gmail.com>
2021-12-08 11:18:34 +0200

    update url

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  config: 'config/config.json',
+  config: 'config/config.release.json',
   scxPath: './assets/StockChartX/scripts/develop/',
   isDev: true,
 };
